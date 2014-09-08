1db0c6c292b08f988f4fabe46ffa318a48a53c72
Dilip Muthukrishnan <dm_iyer@hotmail.com>
2014-09-08 01:46:42 -0400

    Ported over Robot update logic from iOS version

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,23 +4,27 @@ using System.Collections;
 public class Robot : MonoBehaviour
 {
 
-	public float speed;
-	public Vector2 maxVelocity;
 	private Animator animator;
+	private bool jumpEnabled;
+	private float runningSpeed;
+	private float jumpingSpeed;
+	private Vector2 maxVelocity;
 
 	// Use this for initialization
 	void Start ()
 	{
-		float speed = 10.0f;
-		Vector2 maxVelocity = new Vector2(5.0f, 20.0f);
+		runningSpeed = 10.0f;
+		jumpingSpeed = 100.0f;
+		maxVelocity = new Vector2(5.0f, 200.0f);
 		animator = GetComponent<Animator>();
+		jumpEnabled = true;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	/*void Update ()
 	{
-		var forceX = 0.0f;
-		var forceY = 0.0f;
+		float forceX = 0.0f;
+		float forceY = 0.0f;
 		var absVelX = Mathf.Abs(rigidbody2D.velocity.x);
 		var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
 		if (Input.GetKey("right"))
@@ -46,6 +50,130 @@ public class Robot : MonoBehaviour
 			animator.SetInteger("AnimState", 0);
 		}
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
-	}
+	}*/
 
+	// Update is called once per frame
+	void Update()
+	{
+		float forceX = 0.0f;
+		float forceY = 0.0f;
+		float absVelX = Mathf.Abs(rigidbody2D.velocity.x);
+		float absVelY = Mathf.Abs(rigidbody2D.velocity.y);
+		switch (animator.GetInteger("AnimState"))
+		{
+			case 0: //idle
+			{
+				if (Input.GetKey("left") || Input.GetKey("right"))
+				{
+					animator.SetInteger("AnimState", 1); //running
+					if (Input.GetKey("left"))
+					{
+						//flip direction
+						transform.localScale = new Vector3(-1.835181f, 1.835183f, 1.0f);
+					}
+					if (Input.GetKey("right"))
+					{
+						//flip direction
+						transform.localScale = new Vector3(1.835181f, 1.835183f, 1.0f);
+					}
+				}
+				// Don't allow repeated jumps (disable it until
+				// the button is released)
+				if (Input.GetKey("space") && jumpEnabled)
+				{
+					animator.SetInteger("AnimState", 2); //jumping
+					if (absVelY < maxVelocity.y)
+					{
+						forceY = jumpingSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+					//[[OALSimpleAudio sharedInstance] playEffect:@"jump_sound.wav"];
+					jumpEnabled = false;
+				}
+				else if (!Input.GetKey("space"))
+				{
+					jumpEnabled = true;
+				}
+				break;
+			}
+			case 1: //running
+			{
+				if (!(Input.GetKey("left") || Input.GetKey("right")))
+				{
+					animator.SetInteger("AnimState", 0); //idle
+					break;
+				}
+				// Don't allow repeated jumps (disable it until
+				// the button is released)
+				if (Input.GetKey("space") && jumpEnabled)
+				{
+					animator.SetInteger("AnimState", 2); //jumping
+					if (absVelY < maxVelocity.y)
+					{
+						forceY = jumpingSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+					//[[OALSimpleAudio sharedInstance] playEffect:@"jump_sound.wav"];
+					jumpEnabled = false;
+					break;
+				}
+				else if (!Input.GetKey("space"))
+				{
+					jumpEnabled = true;
+				}
+				// Determine direction of motion and move the robot
+				if (Input.GetKey("left"))
+				{
+					transform.localScale = new Vector3(-1.835181f, 1.835183f, 1.0f);
+					if (absVelX < maxVelocity.x)
+					{
+						forceX = -runningSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+				}
+				if (Input.GetKey("right"))
+				{
+					transform.localScale = new Vector3(1.835181f, 1.835183f, 1.0f);
+					if (absVelX < maxVelocity.x)
+					{
+						forceX = runningSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+				}
+				break;
+			}
+			case 2: //jumping
+			{
+				Debug.Log ("Still Jumping");
+				// Determine direction of motion and move the robot
+				if (Input.GetKey("left"))
+				{
+					transform.localScale = new Vector3(-1.835181f, 1.835183f, 1.0f);
+					if (absVelX < maxVelocity.x)
+					{
+						forceX = -runningSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+				}
+				if (Input.GetKey("right"))
+				{
+					transform.localScale = new Vector3(1.835181f, 1.835183f, 1.0f);
+					if (absVelX < maxVelocity.x)
+					{
+						forceX = runningSpeed;
+					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
+				}
+			break;
+		}
+		case 3: //falling
+			{
+				break;
+			}
+			default:
+				// Shouldn't happen
+				break;
+		}
+	}
+	
 }
