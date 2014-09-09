3900edf236f42391db29ced0891669ed1b8e1304
Dilip Muthukrishnan <cheepu82@gmail.com>
2014-09-08 21:28:04 -0400

    Added jumping sound to robot

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -10,18 +10,20 @@ public class Robot : MonoBehaviour
 	private float jumpingSpeed;
 	private float brakeSpeed;
 	private Vector2 maxVelocity;
+	public AudioClip jumpSound;
 
 	// Use this for initialization
 	void Start ()
 	{
 		runningSpeed = 20.0f;
 		jumpingSpeed = 450.0f;
-		brakeSpeed = 30.0f;
+		brakeSpeed = 50.0f;
 		maxVelocity = new Vector2(5.0f, 200.0f);
 		animator = GetComponent<Animator>();
 		jumpEnabled = true;
 	}
 
+	// collision callback
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		if (coll.gameObject.tag == "floor")
@@ -29,38 +31,6 @@ public class Robot : MonoBehaviour
 			animator.SetInteger("AnimState", 0); //idle
 		}
 	}
-	
-	// Update is called once per frame
-	/*void Update ()
-	{
-		float forceX = 0.0f;
-		float forceY = 0.0f;
-		var absVelX = Mathf.Abs(rigidbody2D.velocity.x);
-		var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
-		if (Input.GetKey("right"))
-		{
-			if (absVelX < maxVelocity.x)
-			{
-				forceX = speed;
-			}
-			transform.localScale = new Vector3(1.835181f, 1.835183f, 1.0f);
-			animator.SetInteger("AnimState", 1);
-		} 
-		else if (Input.GetKey("left"))
-		{
-			if (absVelX < maxVelocity.x)
-			{
-				forceX = -speed;
-			}
-			transform.localScale = new Vector3(-1.835181f, 1.835183f, 1.0f);
-			animator.SetInteger("AnimState", 1);
-		}
-		else
-		{
-			animator.SetInteger("AnimState", 0);
-		}
-		rigidbody2D.AddForce(new Vector2(forceX, forceY));
-	}*/
 
 	// Update is called once per frame
 	void Update()
@@ -69,24 +39,32 @@ public class Robot : MonoBehaviour
 		float forceY = 0.0f;
 		float absVelX = Mathf.Abs(rigidbody2D.velocity.x);
 		float absVelY = Mathf.Abs(rigidbody2D.velocity.y);
+		// Change behavior according to the current animation state
 		switch (animator.GetInteger("AnimState"))
 		{
 			case 0: //idle
 			{
-				Debug.Log("Robot is Idle");
 				if (Input.GetKey("left") || Input.GetKey("right"))
 				{
 					animator.SetInteger("AnimState", 1); //running
+					// Determine direction of motion and move the robot
 					if (Input.GetKey("left"))
 					{
-						//flip direction
 						transform.localScale = new Vector3(-1.835181f, 1.835183f, 1.0f);
+						if (absVelX < maxVelocity.x)
+						{
+							forceX = -runningSpeed;
+						}
 					}
 					if (Input.GetKey("right"))
 					{
-						//flip direction
 						transform.localScale = new Vector3(1.835181f, 1.835183f, 1.0f);
+						if (absVelX < maxVelocity.x)
+						{
+							forceX = runningSpeed;
+						}
 					}
+					rigidbody2D.AddForce(new Vector2(forceX, forceY));
 				}
 				// Don't allow repeated jumps (disable it until
 				// the button is released)
@@ -98,7 +76,10 @@ public class Robot : MonoBehaviour
 						forceY = jumpingSpeed;
 					}
 					rigidbody2D.AddForce(new Vector2(forceX, forceY));
-					//[[OALSimpleAudio sharedInstance] playEffect:@"jump_sound.wav"];
+					if (jumpSound)
+					{
+						AudioSource.PlayClipAtPoint(jumpSound, transform.position);
+					}
 					jumpEnabled = false;
 				}
 				else if (!Input.GetKey("space"))
@@ -109,7 +90,6 @@ public class Robot : MonoBehaviour
 			}
 			case 1: //running
 			{
-				Debug.Log("Robot is Running");
 				if (!(Input.GetKey("left") || Input.GetKey("right")))
 				{
 					animator.SetInteger("AnimState", 0); //idle
@@ -135,7 +115,10 @@ public class Robot : MonoBehaviour
 						forceY = jumpingSpeed;
 					}
 					rigidbody2D.AddForce(new Vector2(forceX, forceY));
-					//[[OALSimpleAudio sharedInstance] playEffect:@"jump_sound.wav"];
+					if (jumpSound)
+					{
+						AudioSource.PlayClipAtPoint(jumpSound, transform.position);
+					}
 					jumpEnabled = false;
 					break;
 				}
@@ -166,7 +149,6 @@ public class Robot : MonoBehaviour
 			}
 			case 2: //jumping
 			{
-				Debug.Log("Robot is Jumping");
 				// Determine direction of motion and move the robot
 				if (Input.GetKey("left"))
 				{
