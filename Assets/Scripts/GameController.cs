0ffe096d3ef2ca347475246fbb2be64104f9e099
Dilip Muthukrishnan <cheepu82@gmail.com>
2014-09-13 15:15:11 -0400

    Added a game controller object and trying to experiment with multiple clients connecting at the same time but it is currently broken

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AIPFramework;
+
+public class GameController : AIPEventListener
+{
+	public Transform robot;
+	private string[] clientIDs;
+	private int numOfClients;
+	public string currentMessage = "No message yet";
+	public AIPNetwork myNetwork;
+	private Queue<string> queue;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Create custom network object "myNetwork", connect
+		myNetwork = new AIPNetwork(); 
+		myNetwork.Connect();
+		
+		//View the exposed values in AIPNetwork 
+		//Note that myNetwork.isConnected may return a false value as the 
+		//connection occurs in a separate thread and is time dependant 
+		
+		Debug.Log ("myNetwork.Mobicode: " + myNetwork.Mobicode); 
+		Debug.Log ("myNetwork.isConnected: " + myNetwork.isConnected);
+		clientIDs = new string[20];
+		numOfClients = 0;
+
+		queue = new Queue<string>();
+	}
+
+	//Display the contents of any messages received from clients
+	public override void eventMessage(string name, string data, string clientId)
+	{
+		// Check if this client already exists
+		bool clientExists = false;
+		for (int i = 0; i < numOfClients; i++)
+		{
+			if (clientId == clientIDs[i])
+			{
+				clientExists = true;
+				break;
+			}
+		}
+		if (!clientExists)
+		{
+			Debug.Log ("We received a message from a client: " + data); 
+			Debug.Log("Its name is: "+ name);
+			Debug.Log("It came from: "+ clientId);
+			lock(this)
+			{
+				queue.Enqueue(clientId);
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		//Display the Mobicode and most recent message to the user
+		GUI.Box(new Rect(5,5,400,40),"Mobicode: " + myNetwork.Mobicode); 
+		GUI.Box(new Rect(5,50,400,40), currentMessage); 		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		lock(this)
+		{
+			if (queue.Count > 0)
+			{
+				string clientId = queue.Dequeue();
+				float x = -2f + (float)numOfClients/2f;
+				float y = 1.64f;
+				RobotController clone = Instantiate(robot, new Vector3(x, y, 0), Quaternion.identity) as RobotController;
+				clone.ClientID = clientId;
+				clientIDs[numOfClients] = clientId;
+				numOfClients++;
+			}
+		}
+	}
+}
--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -2,66 +2,52 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-//using AIPFramework;
+using AIPFramework;
 
-//public class RobotController : AIPEventListener
-public class RobotController : MonoBehaviour
+public class RobotController : AIPEventListener
+//public class RobotController : MonoBehaviour
 {
 	private bool LEFT;
 	private bool RIGHT;
 	private bool JUMP;
-	//public AIPNetwork myNetwork;
-	//public string currentMessage = "No message yet";
+	private string clientID;
 
 	// Use this for initialization
-	/*void Start ()
+	void Start ()
 	{
-		//Create custom network object "myNetwork", connect
-		myNetwork = new AIPNetwork(); 
-		myNetwork.Connect();
-		
-		//View the exposed values in AIPNetwork 
-		//Note that myNetwork.isConnected may return a false value as the 
-		//connection occurs in a separate thread and is time dependant 
-		
-		Debug.Log ("myNetwork.Mobicode: " + myNetwork.Mobicode); 
-		Debug.Log ("myNetwork.isConnected: " + myNetwork.isConnected);
-	}*/
+		clientID = "";
+	}
 
-	//Display the contents of any messages received from clients
-	/*public override void eventMessage(string name, string data, string clientId)
+	//Respond to events received by the client
+	public override void eventMessage(string name, string data, string clientId)
 	{
 		Debug.Log ("We received a message from a client: " + data); 
 		Debug.Log("Its name is: "+ name);
 		Debug.Log("It came from: "+ clientId); 
 		
-		//If a client has sent an event, respond to that event
-		if (name == "leftevent") 
+		//Only respond to events from this clientID!
+		if (clientId == clientID)
 		{
-			LEFT = true;
+			if (name == "leftevent") 
+			{
+				LEFT = true;
+			}
+			if (name == "rightevent")
+			{
+				RIGHT = true;
+			}
+			if (name == "jumpevent")
+			{
+				JUMP = true;
+			}
 		}
-		if (name == "rightevent")
-		{
-			RIGHT = true;
-		}
-		if (name == "jumpevent")
-		{
-			JUMP = true;
-		}
-	}*/
+	}
 
-	/*public IEnumerator ResetJumpAfter(float time)
+	public IEnumerator ResetJumpAfter(float time)
 	{
 		yield return new WaitForSeconds(time);
 		JUMP = false;
-	}*/
-	
-	/*void OnGUI()
-	{
-		//Display the Mobicode and most recent message to the user
-		GUI.Box(new Rect(5,5,400,40),"Mobicode: " + myNetwork.Mobicode); 
-		GUI.Box(new Rect(5,50,400,40), currentMessage); 		
-	}*/
+	}
 
 	public bool Left
 	{
@@ -86,13 +72,27 @@ public class RobotController : MonoBehaviour
 			return JUMP;
 		}
 	}
+
+	public string ClientID
+	{
+		//set the clientID
+		set
+		{ 
+			clientID = value;
+		}
+		//get the clientID 
+		get
+		{ 
+			return clientID;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		LEFT = Input.GetKey("left");
+		/*LEFT = Input.GetKey("left");
 		RIGHT = Input.GetKey("right");
-		JUMP = Input.GetKey("space");
-		//StartCoroutine(ResetJumpAfter(0.1f));
+		JUMP = Input.GetKey("space");*/
+		StartCoroutine(ResetJumpAfter(0.1f));
 	}
 }
