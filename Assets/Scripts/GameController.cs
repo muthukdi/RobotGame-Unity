16ac48e06a022cfffb5ddfeacb71cbddd05a7865
Dilip Muthukrishnan <dm_iyer@hotmail.com>
2014-09-21 23:21:26 -0400

    Do not instantiate crawlers dynamically

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,17 +4,11 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
 	public Transform robot;
-	public Transform crawler;
 	
 	// Use this for initialization
 	void Start ()
 	{
 		Instantiate(robot);
-		for (int i = 0; i < 3; i++)
-		{
-			Instantiate(crawler);
-		}
-
 	}
 	
 	// Update is called once per frame
